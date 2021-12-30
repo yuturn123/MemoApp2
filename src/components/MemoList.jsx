3faedbcd136adeb43d6803ea64b40671084a9d4e
yuturn123 <haha.huhu.8yuta@icloud.com>
2021-12-30 22:31:25 +0900

    Fetch data from firestone on MemoDetailScreen

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -7,6 +7,7 @@ import { useNavigation } from '@react-navigation/native';
 import {
   shape, string, instanceOf, arrayOf,
 } from 'prop-types';
+import { dateToString } from '../utils';
 
 export default function MemoList(props) {
   const { memos } = props;
@@ -17,11 +18,11 @@ export default function MemoList(props) {
       <TouchableOpacity
         key={item.id}
         style={styles.memoListItem}
-        onPress={() => { navigation.navigate('MemoDetail'); }}
+        onPress={() => { navigation.navigate('MemoDetail', { id: item.id }); }}
       >
         <View>
           <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
-          <Text style={styles.memoListItemDate}>{String(item.updatedAt)}</Text>
+          <Text style={styles.memoListItemDate}>{dateToString(item.updatedAt)}</Text>
         </View>
         <TouchableOpacity
           style={styles.memoDelete}
